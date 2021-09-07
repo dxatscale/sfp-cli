97c443988274f6b7083366b689df3927f03e0431
azlam-abdulsalam <sfpowerscripts@dxscale>
2021-09-08 01:23:36 +1000

    feat(pull): add org prompt

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -1,62 +1,89 @@
-import {Command, flags} from '@oclif/command'
-import {ComponentSet, MetadataConverter} from '@salesforce/source-deploy-retrieve';
-import path = require('path');
+import { Command, flags } from "@oclif/command";
+import {
+  ComponentSet,
+  MetadataConverter,
+} from "@salesforce/source-deploy-retrieve";
+import path = require("path");
 import * as fs from "fs-extra";
 import child_process = require("child_process");
-import inquirer = require('inquirer');
-inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+import inquirer = require("inquirer");
+inquirer.registerPrompt(
+  "autocomplete",
+  require("inquirer-autocomplete-prompt")
+);
 import ProjectConfig from "@dxatscale/sfpowerscripts.core/lib/project/ProjectConfig";
 const fuzzy = require("fuzzy");
 import * as resource from "../resource.json";
 const Table = require("cli-table");
 import SFPlogger, {
-  COLOR_HEADER
+  COLOR_HEADER,
 } from "@dxatscale/sfpowerscripts.core/lib/logger/SFPLogger";
+import PromptToPickAnOrg from "../prompts/PromptToPickAnOrg";
+import simpleGit, { SimpleGit } from "simple-git";
 
 export default class Pull extends Command {
-  static description = 'pull source from scratch org/sandbox to the project. Provides interactive interface for packaging new metadata.'
+  static description =
+    "pull source from scratch org/sandbox to the project. Provides interactive interface for packaging new metadata.";
 
-  static examples = [
-    `$ sfp pull -u <scratchorg>`
-  ]
+  static examples = [`$ sfp pull -u <scratchorg>`];
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    forceoverwrite: flags.boolean({char: 'f', description: 'ignore conflict warnings and overwrite changes to the project'}),
-    targetusername: flags.string({char: 'u', description: 'username or alias for the target org'})
-  }
+    help: flags.help({ char: "h" }),
+  };
 
+  static args = [{ name: "caller" }];
   // private readonly registry: MetadataRegistry = defaultRegistry;
 
   static hidden = true;
 
   async run() {
-    const {args, flags} = this.parse(Pull);
+    const { args, flags } = this.parse(Pull);
+
+    if (args.caller !== "inner") {
+      SFPlogger.log(
+        COLOR_HEADER(
+          `sfp cli -- The DX@Scale Dev CLI -- ${this.config.version}`
+        )
+      );
+    }
 
 
 
-    SFPlogger.log(
-      COLOR_HEADER(`sfp cli -- The DX@Scale Dev CLI -- ${this.config.version}`)
+    // TODO: Move to property requiresProject: boolean
+    if (!fs.existsSync("sfdx-project.json"))
+      throw new Error(
+        "This command must be run in the root directory of a SFDX project"
+      );
+
+
+    //Intitialize Git
+    const git: SimpleGit = simpleGit();
+    let currentBranch = (await git.branch()).current;
+    let aliasName = currentBranch.split(/[/]+/).pop();
+
+
+
+    //Prompt to Pick a scratch Org
+    let scratchOrgUserName = await new PromptToPickAnOrg({alias:aliasName}).promptForScratchOrgSelection();
+
+
+    const statusResult = this.getStatusResult(
+      scratchOrgUserName,
+      false
     );
-
-   // TODO: Move to property requiresProject: boolean
-   if (!fs.existsSync("sfdx-project.json")) throw new Error("This command must be run in the root directory of a SFDX project");
-
-
-
-
-    const statusResult = this.getStatusResult(flags.targetusername, flags.forceoverwrite);
 
     if (statusResult.length === 0) {
       console.log("No changes found");
       return;
     }
 
-    const remoteAdditions = statusResult.filter((elem) => elem.state === "Remote Add");
+    const remoteAdditions = statusResult.filter(
+      (elem) => elem.state === "Remote Add"
+    );
 
-
-
-    console.log(`Found ${remoteAdditions.length} new metadata components, which require a new home`);
+    console.log(
+      `Found ${remoteAdditions.length} new metadata components, which require a new home`
+    );
     const defaultPackage = ProjectConfig.getDefaultSFDXPackageDescriptor(null);
 
     let result = [];
@@ -65,19 +92,22 @@ export default class Pull extends Command {
       const obj: Instruction = {
         fullName: remoteAddition.fullName,
         type: remoteAddition.type,
-        destination: []
+        destination: [],
       };
 
       let getMoveAction = await this.getMoveAction(obj);
 
-      if(getMoveAction.moveAction === MoveAction.RECOMMENDED) {
+      if (getMoveAction.moveAction === MoveAction.RECOMMENDED) {
         if (resource.types[obj.type].strategy === Strategy.PLUS_ONE) {
           obj.destination.push(...resource.types[obj.type].recommended);
           await this.getPlusOneMoveAction(obj);
         } else if (resource.types[obj.type].strategy === Strategy.DUPLICATE) {
           obj.destination.push(...resource.types[obj.type].recommended);
         } else if (resource.types[obj.type].strategy === Strategy.SINGLE) {
-          await this.getSingleMoveAction(obj, resource.types[obj.type].recommended);
+          await this.getSingleMoveAction(
+            obj,
+            resource.types[obj.type].recommended
+          );
         } else if (resource.types[obj.type].strategy === Strategy.DELETE) {
           // do nothing
         } else {
@@ -95,35 +125,39 @@ export default class Pull extends Command {
       result.push(obj);
     }
 
+    console.log();
+    console.log("Pulling source components...");
+    let pullResult = JSON.parse(
+      child_process.execSync(
+        `sfdx force:source:pull -u ${scratchOrgUserName}} -f --json`,
+        {
+          encoding: "utf8",
+          stdio: "pipe",
+          maxBuffer: 1024 * 1024 * 5,
+        }
+      )
+    ).result;
 
     console.log();
-    console.log('Pulling source components...');
-    let pullResult = JSON.parse(child_process.execSync(
-      `sfdx force:source:pull -u ${flags.targetusername} -f --json`,
-      {
-        encoding: 'utf8',
-        stdio: 'pipe',
-        maxBuffer: 1024*1024*5
-      }
-    )).result;
+    console.log("Successfully pulled source components");
 
-
-    console.log()
-    console.log('Successfully pulled source components');
-
-    console.log()
-    console.log("Moving source components...")
-
+    console.log();
+    console.log("Moving source components...");
 
     for (let elem of result) {
-      let components = pullResult.pulledSource.filter((component) => component.fullName === elem.fullName && component.type === elem.type);
-
+      let components = pullResult.pulledSource.filter(
+        (component) =>
+          component.fullName === elem.fullName && component.type === elem.type
+      );
 
       const converter = new MetadataConverter();
-      const componentSet = ComponentSet.fromSource(components.find((component) => path.extname(component.filePath)===".xml").filePath);
+      const componentSet = ComponentSet.fromSource(
+        components.find(
+          (component) => path.extname(component.filePath) === ".xml"
+        ).filePath
+      );
 
-
-      for(let dest of elem.destination) {
+      for (let dest of elem.destination) {
         if (dest.aliasfy) {
           let files = fs.readdirSync(dest.package);
           let aliases = files.filter((file) => {
@@ -132,44 +166,53 @@ export default class Pull extends Command {
           });
 
           for (let alias of aliases) {
-            await converter.convert(componentSet, 'source', {
-              type: 'merge',
-              mergeWith: ComponentSet.fromSource(path.resolve(dest.package, alias)).getSourceComponents(),
+            await converter.convert(componentSet, "source", {
+              type: "merge",
+              mergeWith: ComponentSet.fromSource(
+                path.resolve(dest.package, alias)
+              ).getSourceComponents(),
               defaultDirectory: path.join(dest.package, alias),
-              forceIgnoredPaths: componentSet.forceIgnoredPaths ?? new Set<string>()
+              forceIgnoredPaths:
+                componentSet.forceIgnoredPaths ?? new Set<string>(),
             });
           }
         } else {
-          await converter.convert(componentSet, 'source', {
-            type: 'merge',
-            mergeWith: ComponentSet.fromSource(path.resolve(dest.package)).getSourceComponents(),
+          await converter.convert(componentSet, "source", {
+            type: "merge",
+            mergeWith: ComponentSet.fromSource(
+              path.resolve(dest.package)
+            ).getSourceComponents(),
             defaultDirectory: dest.package,
-            forceIgnoredPaths: componentSet.forceIgnoredPaths ?? new Set<string>()
+            forceIgnoredPaths:
+              componentSet.forceIgnoredPaths ?? new Set<string>(),
           });
         }
       }
 
-      for(let component of components)
-      {
+      for (let component of components) {
         fs.unlinkSync(component.filePath);
       }
-
     }
 
     console.log("Successfully moved source components");
   }
 
   private async getExistingPackage(obj: Instruction) {
-    let getExistingPackage = await inquirer.prompt([{
-      type: "autocomplete",
-      name: "package",
-      message: "Search for package",
-      source: this.searchExistingPackages,
-      pageSize: 10
-    }]);
+    let getExistingPackage = await inquirer.prompt([
+      {
+        type: "autocomplete",
+        name: "package",
+        message: "Search for package",
+        source: this.searchExistingPackages,
+        pageSize: 10,
+      },
+    ]);
 
-    let packageDescriptor = ProjectConfig.getSFDXPackageDescriptor(null, getExistingPackage.package);
-    obj.destination.push({package: packageDescriptor.path});
+    let packageDescriptor = ProjectConfig.getSFDXPackageDescriptor(
+      null,
+      getExistingPackage.package
+    );
+    obj.destination.push({ package: packageDescriptor.path });
   }
 
   private async getNewpackage(obj: Instruction) {
@@ -179,10 +222,14 @@ export default class Pull extends Command {
         name: "name",
         message: "Input name of the new package",
         validate: (input, answers) => {
-          if (ProjectConfig.getAllPackages(null).find((packageName) => packageName === input)) {
-             return `Package with name ${input} already exists`;
+          if (
+            ProjectConfig.getAllPackages(null).find(
+              (packageName) => packageName === input
+            )
+          ) {
+            return `Package with name ${input} already exists`;
           } else return true;
-        }
+        },
       },
       {
         type: "list",
@@ -190,26 +237,34 @@ export default class Pull extends Command {
         message: `Select position of the new package`,
         loop: false,
         choices: ProjectConfig.getAllPackages(null),
-        pageSize: 10
+        pageSize: 10,
       },
       {
         type: "list",
         name: "position",
         message: "Position",
-        choices: [{ name: "Before", value: "before" }, { name: "After", value: 'after' }]
-      }
+        choices: [
+          { name: "Before", value: "before" },
+          { name: "After", value: "after" },
+        ],
+      },
     ]);
 
-    let indexOfNewPackage = ProjectConfig.getAllPackages(null).findIndex((packageName) => packageName === getNewPackage.anchor);
-    if (getNewPackage.position === "after")
-      indexOfNewPackage++;
+    let indexOfNewPackage = ProjectConfig.getAllPackages(null).findIndex(
+      (packageName) => packageName === getNewPackage.anchor
+    );
+    if (getNewPackage.position === "after") indexOfNewPackage++;
 
-
-    Pull.createNewPackage(null, getNewPackage.name, path.join("src", getNewPackage.name), indexOfNewPackage);
+    Pull.createNewPackage(
+      null,
+      getNewPackage.name,
+      path.join("src", getNewPackage.name),
+      indexOfNewPackage
+    );
 
     fs.mkdirpSync(path.join("src", getNewPackage.name));
 
-    obj.destination.push({package: path.join("src", getNewPackage.name)});
+    obj.destination.push({ package: path.join("src", getNewPackage.name) });
   }
 
   private async getMoveAction(obj: Instruction) {
@@ -228,8 +283,8 @@ export default class Pull extends Command {
       message: `Select additional package`,
       choices: [
         { name: "Existing", value: MoveAction.EXISTING },
-        { name: "New", value: MoveAction.NEW}
-      ]
+        { name: "New", value: MoveAction.NEW },
+      ],
     });
 
     if (getMoveAction.moveAction === MoveAction.EXISTING) {
@@ -241,15 +296,20 @@ export default class Pull extends Command {
     }
   }
 
-  private async getSingleMoveAction(obj: Instruction, recommended: {package: string; aliasfy: boolean}[]) {
+  private async getSingleMoveAction(
+    obj: Instruction,
+    recommended: { package: string; aliasfy: boolean }[]
+  ) {
     const getPackage = await inquirer.prompt({
       type: "list",
       name: "package",
       message: "Select recommended package",
-      choices: recommended.map((elem) => elem.package)
+      choices: recommended.map((elem) => elem.package),
     });
 
-    obj.destination.push(recommended.find((elem) => elem.package === getPackage.package));
+    obj.destination.push(
+      recommended.find((elem) => elem.package === getPackage.package)
+    );
   }
 
   /**
@@ -259,13 +319,14 @@ export default class Pull extends Command {
    * @param pathOfPackage
    * @param indexOfPackage
    */
-   public static createNewPackage(
+  public static createNewPackage(
     projectDirectory: string,
     nameOfPackage: string,
     pathOfPackage: string,
     indexOfPackage: number
   ) {
-    const packageConfig = ProjectConfig.getSFDXPackageManifest(projectDirectory);
+    const packageConfig =
+      ProjectConfig.getSFDXPackageManifest(projectDirectory);
     packageConfig.packageDirectories.forEach((dir) => {
       if (dir.package === nameOfPackage)
         throw new Error(`Package with name ${nameOfPackage} already exists`);
@@ -273,9 +334,13 @@ export default class Pull extends Command {
     const newPackageDescriptor = {
       path: pathOfPackage,
       package: nameOfPackage,
-      versionNumber: "1.0.0.0"
+      versionNumber: "1.0.0.0",
     };
-    packageConfig.packageDirectories.splice(indexOfPackage, 0, newPackageDescriptor);
+    packageConfig.packageDirectories.splice(
+      indexOfPackage,
+      0,
+      newPackageDescriptor
+    );
 
     let pathToProjectConfig: string;
     if (projectDirectory) {
@@ -288,21 +353,27 @@ export default class Pull extends Command {
 
   private getChoicesForMovingMetadata(metadata) {
     if (resource.types[metadata.type]?.strategy) {
-      let recommendedPackages = resource.types[metadata.type].recommended?.map(elem => elem.package);
+      let recommendedPackages = resource.types[metadata.type].recommended?.map(
+        (elem) => elem.package
+      );
       return [
-        { name: `Recommended (Strategy: ${resource.types[metadata.type].strategy}) ${recommendedPackages ? recommendedPackages : ""}`, value: MoveAction.RECOMMENDED },
+        {
+          name: `Recommended (Strategy: ${
+            resource.types[metadata.type].strategy
+          }) ${recommendedPackages ? recommendedPackages : ""}`,
+          value: MoveAction.RECOMMENDED,
+        },
         { name: "Existing", value: MoveAction.EXISTING },
-        { name: "New", value: MoveAction.NEW},
+        { name: "New", value: MoveAction.NEW },
         { name: "Do nothing", value: MoveAction.NOTHING },
       ];
     } else {
       return [
         { name: "Existing", value: MoveAction.EXISTING },
-        { name: "New", value: MoveAction.NEW},
+        { name: "New", value: MoveAction.NEW },
         { name: "Do nothing", value: MoveAction.NOTHING },
       ];
     }
-
   }
 
   /**
@@ -314,7 +385,8 @@ export default class Pull extends Command {
   private searchExistingPackages(answers, input) {
     let packages = ProjectConfig.getAllPackages(null);
 
-    const defaultPackage = ProjectConfig.getDefaultSFDXPackageDescriptor(null).package;
+    const defaultPackage =
+      ProjectConfig.getDefaultSFDXPackageDescriptor(null).package;
     packages = packages.filter((packageName) => packageName !== defaultPackage);
 
     if (input) {
@@ -327,21 +399,28 @@ export default class Pull extends Command {
     let resultJson = child_process.execSync(
       `sfdx force:source:status -u ${targetOrg} --json`,
       {
-        encoding: 'utf8',
-        stdio: 'pipe',
-        maxBuffer: 1024*1024*5
+        encoding: "utf8",
+        stdio: "pipe",
+        maxBuffer: 1024 * 1024 * 5,
       }
     );
 
     let result = JSON.parse(resultJson);
 
-    const conflicts = result.result.filter((elem) => elem.state.endsWith("(Conflict)"));
+    const conflicts = result.result.filter((elem) =>
+      elem.state.endsWith("(Conflict)")
+    );
 
     if (conflicts.length > 0 && !isForceOverwrite) {
       this.printStatus(conflicts);
-      throw new Error("Source conflict(s) detected. Verify that you want to keep the remote versions, then run 'sfp pull -f' with the --forceoverwrite (-f) option");
+      throw new Error(
+        "Source conflict(s) detected. Verify that you want to keep the remote versions, then run 'sfp pull -f' with the --forceoverwrite (-f) option"
+      );
     } else {
-      statusResult = result.result.filter((elem) => !elem.state.endsWith("(Conflict)") && !elem.state.startsWith("Local"));
+      statusResult = result.result.filter(
+        (elem) =>
+          !elem.state.endsWith("(Conflict)") && !elem.state.startsWith("Local")
+      );
       this.printStatus(statusResult);
     }
 
@@ -350,11 +429,16 @@ export default class Pull extends Command {
 
   private printStatus(statusResult) {
     const table = new Table({
-      head: ["State", "Full Name", "Type", "File Path"]
+      head: ["State", "Full Name", "Type", "File Path"],
     });
 
     statusResult.forEach((elem) => {
-      table.push([elem.state, elem.fullName, elem.type, elem.filePath ? elem.filePath : "N/A"]);
+      table.push([
+        elem.state,
+        elem.fullName,
+        elem.type,
+        elem.filePath ? elem.filePath : "N/A",
+      ]);
     });
 
     console.log(table.toString());
@@ -433,14 +517,14 @@ enum MoveAction {
   RECOMMENDED = "recommended",
   NEW = "new",
   EXISTING = "existing",
-  NOTHING = "nothing"
+  NOTHING = "nothing",
 }
 
 enum Strategy {
   SINGLE = "single",
   DUPLICATE = "duplicate",
   PLUS_ONE = "plus-one",
-  DELETE = "delete"
+  DELETE = "delete",
 }
 
 // interface Component extends MetadataType {
@@ -450,7 +534,7 @@ enum Strategy {
 // }
 
 interface Instruction {
-  fullName: string,
-  type: string,
-  destination: {package: string; aliasfy?: boolean}[]
+  fullName: string;
+  type: string;
+  destination: { package: string; aliasfy?: boolean }[];
 }
