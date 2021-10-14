bac343d4b43df6ddce4dcc4e560ff73e88df445c
Alan Ly <alan.ly@accenture.com>
2021-10-14 12:01:30 +1100

    Show status of change in commit workflow

--- src/workflows/git/CommitWorkflow.ts
+++ src/workflows/git/CommitWorkflow.ts
@@ -19,16 +19,18 @@ export default class CommitWorkflow {
     // track files so that they are included in diff
     await this.git.raw(["add", "--intent-to-add", "--", ...paths]);
 
-    const unstagedFiles = (await this.git.diff(["--name-only", "--", ...paths])).split("\n");
-    unstagedFiles.pop(); // Remove empty string at the end of the array
+    let diffResult = (await this.git.diff(["--name-status", "--", ...paths])).split("\n");
+    diffResult.pop(); // Remove empty string at the end of the array
+
+    const unstagedChanges = this.tokenizeDiffResult(diffResult)
 
     const filesToStage = await inquirer.prompt([
       {
         type: "checkbox",
         name: "files",
         message: "Select files to commit",
-        choices: unstagedFiles,
-        default: unstagedFiles,
+        choices: this.getChoices(unstagedChanges),
+        default: this.getChoices(unstagedChanges).map(choice => choice.value),
         loop: false
       }
     ]);
@@ -66,5 +68,26 @@ export default class CommitWorkflow {
       SFPLogger.log("Committing changes in package directories...");
       await this.git.commit(commitMessage, paths);
     }
+  }
+
+  private getChoices(tokenizedDiffResult: {status: string, file: string}[]) {
+    return tokenizedDiffResult.map(change => {
+      return {
+        name: change.status + "   " + change.file,
+        value: change.file
+      }
+    });
+  }
+
+  private tokenizeDiffResult(diffResult: string[]) {
+    return diffResult.map(elem => {
+      const status = elem.charAt(0);
+      const file = elem.slice(1).trim();
+
+      return {
+        status: status,
+        file: file
+      };
+    });
   }
 }
