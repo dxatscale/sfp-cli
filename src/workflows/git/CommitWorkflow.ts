7e54d4d31eacde1a2b41ee298f5459de77d3195a
Alan Ly <36793262+aly76@users.noreply.github.com>
2021-10-14 13:30:44 +1100

    Show status of change in commit workflow (#41)
    * Show status of change in commit workflow
    
    * Move to method

--- src/workflows/git/CommitWorkflow.ts
+++ src/workflows/git/CommitWorkflow.ts
@@ -16,19 +16,15 @@ export default class CommitWorkflow {
       .map((elem) => elem.path);
     paths.push("sfdx-project.json");
 
-    // track files so that they are included in diff
-    await this.git.raw(["add", "--intent-to-add", "--", ...paths]);
-
-    const unstagedFiles = (await this.git.diff(["--name-only", "--", ...paths])).split("\n");
-    unstagedFiles.pop(); // Remove empty string at the end of the array
+    const unstagedChanges = await this.getUnstagedChanges(paths);
 
     const filesToStage = await inquirer.prompt([
       {
         type: "checkbox",
         name: "files",
         message: "Select files to commit",
-        choices: unstagedFiles,
-        default: unstagedFiles,
+        choices: this.getChoices(unstagedChanges),
+        default: this.getChoices(unstagedChanges).map(choice => choice.value),
         loop: false
       }
     ]);
@@ -66,5 +62,36 @@ export default class CommitWorkflow {
       SFPLogger.log("Committing changes in package directories...");
       await this.git.commit(commitMessage, paths);
     }
+  }
+
+  private getChoices(tokenizedDiffResult: {status: string, file: string}[]) {
+    return tokenizedDiffResult.map(change => {
+      return {
+        name: change.status + "   " + change.file,
+        value: change.file
+      }
+    });
+  }
+
+  private async getUnstagedChanges(paths: string[], ) {
+    // track files so that they are included in diff
+    await this.git.raw(["add", "--intent-to-add", "--", ...paths]);
+
+    let diffResult = (await this.git.diff(["--name-status", "--", ...paths])).split("\n");
+    diffResult.pop(); // Remove empty string at the end of the array
+
+    return this.tokenizeDiffResult(diffResult)
+  }
+
+  private tokenizeDiffResult(diffResult: string[]) {
+    return diffResult.map(elem => {
+      const status = elem.charAt(0);
+      const file = elem.slice(1).trim();
+
+      return {
+        status: status,
+        file: file
+      };
+    });
   }
 }
