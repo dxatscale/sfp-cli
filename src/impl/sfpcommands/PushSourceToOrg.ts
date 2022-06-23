2fab956eab26b26dc425a893f5f647ae7c167e2e
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-06-23 13:58:14 +1000

    feat(sync): add an option for direct push

--- src/impl/sfpcommands/PushSourceToOrg.ts
+++ src/impl/sfpcommands/PushSourceToOrg.ts
@@ -1,5 +1,5 @@
 import cli from 'cli-ux';
-import { ConsoleLogger, COLOR_KEY_VALUE } from '@dxatscale/sfpowerscripts.core/lib/logger/SFPLogger';
+import SFPLogger, { ConsoleLogger, COLOR_KEY_VALUE } from '@dxatscale/sfpowerscripts.core/lib/logger/SFPLogger';
 import SourcePush from '../sfdxwrappers/SourcePush';
 import PushErrorDisplayer from '@dxatscale/sfpowerscripts.core/lib/display/PushErrorDisplayer';
 
@@ -9,7 +9,7 @@ export default class PushSourceToOrg {
     async exec() {
         try {
             cli.action.start(`  Pushing source to org ${COLOR_KEY_VALUE(this.devOrg)}`);
-            await new SourcePush(this.devOrg, true).exec();
+            await new SourcePush(this.devOrg, true).exec(true,0,false);
             cli.action.stop();
         } catch (error) {
             PushErrorDisplayer.printMetadataFailedToPush(JSON.parse(error.message), new ConsoleLogger());
