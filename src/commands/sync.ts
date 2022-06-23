c1228a5d31d63c6c51dfd9c3a0d8916c61fbbd88
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-06-23 13:58:14 +1000

    feat(sync): add an option for direct push

--- src/commands/sync.ts
+++ src/commands/sync.ts
@@ -6,6 +6,7 @@ import simpleGit from 'simple-git';
 import PickAnOrgWorkflow from '../workflows/org/PickAnOrgWorkflow';
 import SyncGit from '../workflows/sync/SyncGit';
 import SyncOrg from '../workflows/sync/SyncOrg';
+import PushSourceToOrg from '../impl/sfpcommands/PushSourceToOrg';
 
 export default class Sync extends CommandsWithInitCheck {
     static description = 'sync changes effortlessly either with repository or development environment';
@@ -40,6 +41,28 @@ export default class Sync extends CommandsWithInitCheck {
             }
 
             await new SyncOrg(git, this.sfpProjectConfig, devOrg).execute();
+        } else if(option == 'sync-org-force')
+        {
+            const branches = await git.branch();
+            const workItem = this.sfpProjectConfig.getWorkItemGivenBranch(branches.current);
+
+            //Only select org if there is no org available
+            let devOrg: string;
+            if (workItem?.defaultDevOrg == null) {
+                SFPLogger.log(
+                    `  ${COLOR_WARNING(
+                        `Work Item not intialized, always utilize ${COLOR_KEY_MESSAGE(`sfp work`)} to intialize work`
+                    )}`
+                );
+                devOrg = await new PickAnOrgWorkflow().getADevOrg();
+                //Reset source tracking when user picks up random orgs
+                //await new SourceTrackingReset(devOrg).exec(true);
+            } else {
+                devOrg = workItem.defaultDevOrg;
+            }
+
+
+            await new PushSourceToOrg(devOrg).exec();
         }
     }
 
@@ -52,6 +75,7 @@ export default class Sync extends CommandsWithInitCheck {
                 choices: [
                     { name: 'Sync local with remote repository', value: 'sync-git' },
                     { name: 'Sync local with Dev Org', value: 'sync-org' },
+                    { name: 'Force Push to Dev Org', value: 'sync-org-force' },
                 ],
                 default: 'Sync local with remote repository',
             },
